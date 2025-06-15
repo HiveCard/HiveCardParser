39930c5ec29b8b2e92b81de3d850847f5bb0b48b
Precious Barro <precious.barro@essigplm.com>
2025-06-16 00:33:40 +0800

    automate get card#

---
+++ HiveCard.PdfParser/Parsers/MetrobankExtractor.cs
@@ -0,0 +1,24 @@
+﻿using HiveCard.PdfParser.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HiveCard.PdfParser.Models;
+using HiveCard.PdfParser.Helpers;
+
+namespace HiveCard.PdfParser.Parsers
+{
+    //private readonly int[] _accSummaryCoordinates = new int[] { /* update if needed */ };
+   // private readonly int[] _breakDownListCoordinates = new int[] { /* update if needed */ };
+    internal class MetrobankExtractor : ExtractorHelper, IExtractor
+    {
+        public BankStatement Run(string pdfPath)
+        {
+            var bankStatement = new BankStatement();
+
+            return bankStatement;
+        }
+
+    }
+}
